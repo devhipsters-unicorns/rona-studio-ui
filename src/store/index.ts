cf60350dea11a587df45cfe0568616e9ceb6c6a0
ziggleton <ziggy@devhipsters.co.za>
2021-10-22 17:02:07 +0200

    renamed add, added message store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { createStore, Store } from 'vuex'
 
 import user from './modules/User'
+import message from './modules/Message'
 
 const store: Store<any> = createStore({
   modules: {
     user,
+    message,
   },
 })
 
